60237f4c22d194146a9ed7a45b97fed090ea5042
EmmanuelIbiyemi <ibiyemiemmanuel68@gmail.com>
2025-08-15 16:50:37 +0100

    Initial commit

--- db/Database.py
+++ db/Database.py
@@ -1,2 +1,9 @@
 import sqlite3 as sql
 
+database = sql.connect('quickproject.db')
+database.close()
+
+if (database):
+    print("DataBase have been created!")
+else:
+    print("Their was error while creating the database")
